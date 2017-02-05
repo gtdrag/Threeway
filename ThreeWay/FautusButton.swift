e027394102dc962df558b055b6d81bed21c6470f
gtdrag <gdrag@redfoundry.com>
2017-02-05 14:20:34 -0600

    added slight delay to improve button touch experience

--- ThreeWay/FautusButton.swift
+++ ThreeWay/FautusButton.swift
@@ -1,6 +1,5 @@
 //
 //  FautusButton.swift
-//  ThreeWay
 //
 //  Created by George Drag on 2/4/17.
 //  Copyright © 2017 George Drag. All rights reserved.
@@ -12,18 +11,20 @@ import UIKit
 
 @IBDesignable public class FautusButton: UIControl {
     
-    @IBInspectable var fautusButtonText: String = "" {
+    @IBInspectable var fautusButtonText: String = "submit" {
         willSet {
             fautusButtonLabel.text = fautusButtonText.uppercased()
+            fautusButtonLabel.kern(kerningValue: 2)
+            
         }
     }
     
-@IBOutlet weak var fautusButtonLabel: UILabel!
+    @IBOutlet weak var fautusButtonLabel: UILabel!
     
     @IBOutlet weak var caretImage: UIImageView!
     @IBOutlet weak var fautusButton: UIButton!
     
-     override init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         xibSetup()
     }
@@ -44,12 +45,24 @@ import UIKit
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: "FautusButton", bundle: bundle)
         let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
-        fautusButtonLabel.kern(kerningValue: 1.5)
         return view
     }
     
-    
     @IBAction func fautusButtonTouchDown(_ sender: Any) {
+        highlightButtton()
+        
+    }
+    
+    
+    @IBAction func fautusButtonTouchUp(_ sender: Any) {
+        let deadlineTime = DispatchTime.now() + 0.1
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
+            self.resetButtton()
+        }
+        self.sendActions(for: UIControlEvents.valueChanged)
+    }
+    
+    func highlightButtton() {
         fautusButtonLabel.textColor = UIColor.darkGray
         fautusButton.backgroundColor = UIColor(red: 0.02, green: 0.05, blue: 0.02, alpha: 0.05)
         let frm: CGRect = caretImage.frame
@@ -58,11 +71,10 @@ import UIKit
         let rect = CGRect(x: x, y: y, width: 17, height: 17)
         caretImage.frame = rect
         fautusButtonLabel.font = fautusButtonLabel.font.withSize(16.5)
-
+        
     }
-
     
-    @IBAction func fautusButtonTouchUp(_ sender: Any) {
+    func resetButtton() {
         fautusButtonLabel.textColor = UIColor.black
         fautusButton.backgroundColor = UIColor.clear
         let frm: CGRect = caretImage.frame
@@ -71,8 +83,7 @@ import UIKit
         let rect = CGRect(x: x, y: y, width: 16, height: 16)
         caretImage.frame = rect
         fautusButtonLabel.font = fautusButtonLabel.font.withSize(16)
-        self.sendActions(for: UIControlEvents.valueChanged)
     }
-
-   
+    
 }
+
